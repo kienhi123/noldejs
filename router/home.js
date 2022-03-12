492c2b4740bf3cff499afa0ea1810ec9b01e9e67
kienhi123 <kien123@gmail.com>
2022-03-12 10:14:41 +0700

    update:contrller

--- router/home.js
+++ router/home.js
@@ -1,54 +1,31 @@
-import express from "express";
-const router = express.Router();
-import { checkAuth } from "../middeawe/checkAuth";
-const check = (req, res, next) => {
-    const status = true;
-    if(status) {
-    console.log("Chào sếp");
-    next();
-    } else {
-        console.log("Anh không có quyền truy cập");
-    }
- }
+import { Router } from 'express';
+import { create,read, remove, update } from '../controller/products';
+import { checkAuth } from '../middlewares/checkAuth' 
+const router = Router();
+// fake data
+const products = [
+    {id: 1, name: "Product A"},
+    {id: 2, name: "Product B"}
+];
 
- router.post('/products/:id', checkAuth, (req, res) => {
-     console.log(req.body)
-    const products = [
-        {id: 1, name: "Product A"},
-        {id: 2, name: "Product B"}
-    ];
+router.get('/products', checkAuth, (req, res) => {
+    res.json(products);
+});
+router.get('/product/:id', checkAuth, (req, res) => {
+    res.json(products.find(item => item.id === +req.params.id));
+});
+router.post('/products', checkAuth, (req, res) => {
     products.push(req.body);
     res.json(products);
+});
+router.delete('/product/:id', checkAuth, (req, res) => {
+    res.json(products.filter(item => item.id !== +req.params.id));
+});
+// resful API
+router.get('/products', checkAuth, list);
+router.get('/product/:id', checkAuth, read);
+router.post('/products', checkAuth,);
+router.delete('/product/:id', checkAuth, remove);
+router.patch("/product/:id", checkAuth, update )
 
- })
-
- router.get('products/:id', checkAuth, (req, res) => {
-    const products = [
-        {id: 1, name: "Product A"},
-        {id: 2, name: "Product B"}
-    ];
-   
-    res.json(product.find(item => item.id === +req.params.id));
- })
- router.post('/products', checkAuth, (req, res) => {
-    console.log(req.body)
-   const products = [
-       {id: 1, name: "Product A"},
-       {id: 2, name: "Product B"}
-   ];
-   products.push(req.body);
-   res.json(products);
-})
-
-router.delete('/products/:id', checkAuth, (req, res) => {
-    
-   const products = [
-       {id: 1, name: "Product A"},
-       {id: 2, name: "Product B"}
-   ];
-   
-   res.json(products.filter(item => item.id == +req.params.id));
-})
-
-
-export default router;
+export default router; 
