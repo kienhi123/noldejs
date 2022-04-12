b8fb3a3b1bcd250610ccb2b8b7c93c5cee94c341
kienhi123 <kien123@gmail.com>
2022-04-13 00:00:15 +0700

    update:search

--- controller/auth.js
+++ controller/auth.js
@@ -1,32 +1,32 @@
 import User from "../model/user";
+import jwt from "jsonwebtoken";
 // Đăng kí
 export const signup = async (req,res) => {
-    const {email,name,password}=  req.body
+    const {email,name,password}=  req.body // Nhận dữ liệu từ cailent nên server
     try {
-        const exitUser = await User.findOne({email}).exec();
+        const exitUser = await User.findOne({email}).exec(); // kiểm tra email vừa nhận đã được đăng kí hay chưa
         if(exitUser){
            res.json({
                message:"Email của bạn đã tồn tại"
            })
         };
-        const user = await new User({email,name,password}).save();
-        res.json({
+        // Sau khi kiểm tra xong nếu chưa đăng kí thì next bước tiếp theo
+        const user = await new User({email,name,password}).save(); // save thực thi câu lệnh vừa nhập từ server
+        res.json({   // Trả về dữ liệu
             user:{
                 _id :user._id,
                 email:user.email,
-                name:user.name
-            }
+                name:user.name,
+                role:user.role
+
+            }  
         })
-    } catch (error) {
-        
+    } catch (error) {     
     }
 }
 // Đăng nhập
-
 export const signin = async (req,res)=>{
     const {email,password}= req.body;
-
-
     try {
         const user = await User.findOne({email}).exec();
         if(!user){
@@ -39,13 +39,16 @@ export const signin = async (req,res)=>{
                 message:"Sai thông tin mật khẩu"
             })
         }
+        const token = jwt.sign({_id:user.id},'123456',{expiresIn: 60*60});
 
     res.json({
         message:"Bạn đã đăng nhập thành công",
-        user:{
-            _id:user._id,
+        token,
+        user:{     
+        _id:user._id,
             email:user.email,
-            name:user.name
+            name:user.name,
+            role:user.role
         }
     })
     } catch (error) {
